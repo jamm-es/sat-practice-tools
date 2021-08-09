0c8ec749a0c2c04b5e0ee37377ff31bb738ede7e
James <bfcda8d7@gmail.com>
2021-08-08 22:06:39 -0400

    make saq input numeric ACTUALLY only on mobile

--- src/grade/Question.jsx
+++ src/grade/Question.jsx
@@ -154,7 +154,6 @@ export default class Question extends React.Component {
               onChange={this.handleSAQInput.bind(this)}
               disabled={this.props.graded}
               ref={this.props.myRef}
-              type='number'
               inputMode={isMobile ? 'numeric' : ''}
             />
           </>
